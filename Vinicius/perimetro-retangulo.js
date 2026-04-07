4615f811e1c1dfb3d93c15c3397e33cb6ad13f69
VINICIUS AZEDIAS VIEIRA <a96198896@rn.senac.local>
2026-04-07 14:28:48 -0300

    feat: Adicionando função para calculo do perimetro do retangulo

---
+++ Vinicius/perimetro-retangulo.js
@@ -0,0 +1,10 @@
+
+function perimetroRetangulo(base, altura) {
+    return 2*(base + altura)
+}
+
+// let perimetro = perimetroRetangulo(20, 2)
+
+console.log(perimetroRetangulo(10, 2))
+
+module.exports = perimetroRetangulo
