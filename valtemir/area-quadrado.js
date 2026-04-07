ea7ffefc4871f13511c9c4b6ec452f3f3ddbdd0e
vlimap <valtemir.procopio@gmail.com>
2026-04-07 14:32:11 -0300

    feat: adicionando função para calculo de teste

---
+++ valtemir/area-quadrado.js
@@ -0,0 +1,11 @@
+// codigo de exemplo
+
+
+function perimentroTriangulo(l1, l2, l3) {
+    const perimentro = l1 + l2 + l3
+    return perimentro
+}
+
+console.log(perimentroTriangulo(3, 4, 5))
+
+module.exports = perimentroTriangulo;
